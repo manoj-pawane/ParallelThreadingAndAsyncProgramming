8d5ae300a9b81ddab21806edf3d8eed25870f3c3
Manoj <manoj.pawane@bridgelabz.com>
2020-10-21 14:18:59 +0530

    [Manoj] Async Programming added

---
+++ AsyncProgramming/Message.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncProgramming
+{
+    public class Message
+    {
+        
+        public string Greeting(string name)
+        {
+            Thread.Sleep(3000);
+
+            return string.Format("Hello, {0}", name);
+
+        }
+
+        public Task<string> GreetingAsync(string name)
+        {
+            Message cooking1 = new Message();
+            return Task.Run<string>(() =>
+            {
+                return cooking1.Greeting(name);
+            });
+        }
+    }
+
+    
+}
---
+++ AsyncProgramming/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncProgramming
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            CallWithAsync();
+            Console.WriteLine("Hello world");
+            Thread.Sleep(15000);
+        }
+
+        private async static void CallWithAsync()
+        {
+            Message cooking = new Message();
+            //some other tasks
+            string result = await cooking.GreetingAsync("Sam");
+            Console.WriteLine(result);
+
+
+            string result1 = cooking.Greeting("Mike");
+            Console.WriteLine(result1);
+
+
+            string result2 = await cooking.GreetingAsync("John");
+            Console.WriteLine(result2);
+        }
+
+
+
+    }
+}
